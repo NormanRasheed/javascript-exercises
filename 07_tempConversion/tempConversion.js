b9620f18767e2ae40e4e49fd0c0c60be78c246cb
Rasheed Norman <rasheednorman5@gmail.com>
2024-10-28 15:19:15 -0400

    Finished assignments 6-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  formula = (num - 32) * 5/9;
+  rounded = Math.round(formula * 10) / 10
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  formula = (num * 9/5) + 32;
+  rounded = Math.round(formula * 10) / 10
+  return rounded;
 };
+
+console.log(convertToCelsius(28));
 
 // Do not edit below this line
 module.exports = {
